1e09c4e27926141341d3551cfaba86a769ea3e29
Raj Choudhary <raaz02256@gmail.com>
2023-03-03 09:23:46 +0000

    React-match-game-assignment5

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import './index.css'
+
+const Header = props => {
+  const {timer, score} = props
+
+  return (
+    <div className="header-main-container">
+      <img
+        className="website-logo"
+        src="https://assets.ccbp.in/frontend/react-js/match-game-website-logo.png"
+        alt="website logo"
+      />
+      <div className="timer-container">
+        <p className="card-length">score:{score}</p>
+        <img
+          className="timer"
+          src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png "
+          alt="timer"
+        />
+        <p className="card-length">
+          {timer} <span className="spam-text">Sec</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+export default Header
